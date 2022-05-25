3380a5bb5a8bc45b42cc4364233946d70a180412
NavjotRathee <93029951+NavjotRathee@users.noreply.github.com>
2022-05-25 17:36:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <h2 className="header--title">Business Card Templates</h2>
+            <h2 className="header--title">Business Card Template</h2>
             <h4 className="header--project">NAVJOT RATHEE's Card</h4>
         </header>
     )
